1d3b71b58b59edf5001340e7954b7e6b9951312e
Dhiraj <50257030+DhirajOli@users.noreply.github.com>
2022-12-05 15:10:33 +0545

    Update overloading.java

--- overloading.java
+++ overloading.java
@@ -1,8 +1,14 @@
 //parent class
 class MyMath {
 //used for main class
+
+//static methods to return the value
 static int add(int x, int y) {
         return x + y;
+}
+
+static int adds(int x, int y,int z) {
+        return x + y + z;
 }
 //method
         void adds() {
@@ -10,12 +16,19 @@ static int add(int x, int y) {
         System.out.println("Integer Value: " + b);
         }
 
+        void addss() {
+        int b = adds(8, 5,4);
+        System.out.println("Integer Value: " + b);
+        }
+
 }
+//main class and main methods
 public class overloading {
 public static void main(String[] args) {
     //object
     MyMath a = new MyMath();
     //output
     a.adds();
+    a.addss();
 }
 }
